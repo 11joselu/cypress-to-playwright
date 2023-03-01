a2e3cc7bacd9b80f5f6807b499c92bc0923873ba
Jose Cabrera <jose@codium.team>
2023-03-02 00:10:03 +0100

    Parse a JavaScript code

--- tests/converter.spec.ts
+++ tests/converter.spec.ts
@@ -2,12 +2,17 @@ import { describe, it } from 'node:test';
 import * as assert from 'assert';
 
 function convert(s: string) {
-  return '';
+  return s;
 }
 
 describe('Converter', () => {
   it('Returns empty string when there are not code', () => {
     const result = convert('');
     assert.strictEqual(result, '');
+  });
+
+  it('Parse a JavaScript code', () => {
+    const result = convert(`function hello() {}`);
+    assert.strictEqual(result, 'function hello() {}');
   });
 });
