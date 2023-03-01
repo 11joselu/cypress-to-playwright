b337fad41fe5aef1d183136beecfb1275f60a1e7
Jose Cabrera <jose@codium.team>
2023-03-02 00:08:46 +0100

    Remove app file

--- src/app.ts
+++
@@ -1,3 +0,0 @@
-export const itWorks = () => {
-  return true;
-};
--- tests/app.spec.ts
+++
@@ -1,9 +0,0 @@
-import { describe, it } from 'node:test';
-import * as assert from 'assert';
-import { itWorks } from '../src/app';
-
-describe('Cypress-to-Playwright', () => {
-  it('It works', () => {
-    assert.strictEqual(itWorks(), true);
-  });
-});
