98fc77de9cf45d654b8abdb40968b1b374b007e6
Jose Cabrera <jose@codium.team>
2023-03-02 21:02:53 +0100

    Do not replace fn.visit by awaited page.goto

--- tests/converter.cypress.spec.ts
+++ tests/converter.cypress.spec.ts
@@ -4,9 +4,15 @@ import { convert } from '../src/convert';
 import { format } from './format';
 
 describe('Converter: Cypress', () => {
-  it('Replace cy.visit to awaited page.goto', () => {
+  it('Replace cy.visit by awaited page.goto', () => {
     const result = convert('cy.visit("http://localhost")');
 
     assert.strictEqual(format(result), 'await page.goto("http://localhost")');
+  });
+
+  it('Do not replace fn.visit by awaited page.goto', () => {
+    const result = convert('fn.visit("http://localhost")');
+
+    assert.strictEqual(format(result), 'fn.visit("http://localhost")');
   });
 });
