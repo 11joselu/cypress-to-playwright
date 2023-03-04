9b0fd6e2190867f0db31402574a1473f0b8bbcd5
Jose Cabrera <jose@codium.team>
2023-03-04 21:32:53 +0100

    Use transform word instead replace

--- test/converter.cypress.test.ts
+++ test/converter.cypress.test.ts
@@ -4,25 +4,25 @@ import { converter } from '../src/converter';
 import { format } from './format';
 
 describe('Converter: Cypress commands', { concurrency: true }, () => {
-  it('Replace cy.visit by awaited page.goto', () => {
+  it('Transform cy.visit by awaited page.goto', () => {
     const result = converter('cy.visit("http://localhost")');
 
     assert.strictEqual(format(result), format('await page.goto("http://localhost")'));
   });
 
-  it('Do not replace fn.visit', () => {
+  it('Do not transform fn.visit', () => {
     const result = converter('fn.visit("http://localhost")');
 
     assert.strictEqual(format(result), format('fn.visit("http://localhost")'));
   });
 
-  it('Do not replace cy.fn(selector).click()', () => {
+  it('Do not transform cy.fn(selector).click()', () => {
     const result = converter('cy.fn("selector").click()');
 
     assert.strictEqual(format(result), format('cy.fn("selector").click()'));
   });
 
-  it('Do not replace cy.fn(selector).type()', () => {
+  it('Do not transform cy.fn(selector).type()', () => {
     const result = converter('cy.fn("selector").type()');
 
     assert.strictEqual(format(result), format('cy.fn("selector").type()'));
@@ -38,19 +38,19 @@ describe('Converter: Cypress commands', { concurrency: true }, () => {
     createOption('uncheck()', 'uncheck()'),
     createOption('uncheck({force: true})', 'uncheck({force: true})'),
   ].forEach((option) => {
-    it(`Replace cy.get(selector).${option.cy} by await page.locator("selector").${option.playwright})`, () => {
+    it(`Transform cy.get(selector).${option.cy} by await page.locator("selector").${option.playwright})`, () => {
       const result = converter(`cy.get("selector").${option.cy}`);
 
       assert.strictEqual(format(result), format(`await page.locator("selector").${option.playwright}`));
     });
 
-    it(`Replace cy.get("selector").first().${option.cy} by awaited page.locator("selector").first().${option.cy}`, () => {
+    it(`Transform cy.get("selector").first().${option.cy} by awaited page.locator("selector").first().${option.cy}`, () => {
       const result = converter('cy.get("selector").first().click()');
 
       assert.strictEqual(format(result), format('await page.locator("selector").first().click();'));
     });
 
-    it(`Replace cy.get("selector").last().${option.cy} by awaited page.locator("selector").last().${option.cy}`, () => {
+    it(`Transform cy.get("selector").last().${option.cy} by awaited page.locator("selector").last().${option.cy}`, () => {
       const result = converter('cy.get("selector").last().click()');
 
       assert.strictEqual(format(result), format('await page.locator("selector").last().click();'));
@@ -67,19 +67,19 @@ describe('Converter: Cypress validation with .should', () => {
     createOption('should("have.value", "a text message")', 'toHaveValue("a text message")'),
     createOption('should("contain", "a text message")', 'toContainText("a text message")'),
   ].forEach((option) => {
-    it(`Replace cy.get().${option.cy} by  cy.get().${option.playwright}`, () => {
+    it(`Transform cy.get().${option.cy} by  cy.get().${option.playwright}`, () => {
       const result = converter(`cy.get("selector").${option.cy}`);
 
       assert.strictEqual(format(result), format(`await expect(page.locator("selector")).${option.playwright}`));
     });
 
-    it(`Replace cy.get().first().${option.cy} by page.locator().first().${option.playwright}`, () => {
+    it(`Transform cy.get().first().${option.cy} by page.locator().first().${option.playwright}`, () => {
       const result = converter(`cy.get("selector").first().${option.cy}`);
 
       assert.strictEqual(format(result), format(`await expect(page.locator("selector").first()).${option.playwright}`));
     });
 
-    it(`Replace cy.get().last().${option.cy} by page.locator().last().${option.playwright}`, () => {
+    it(`Transform cy.get().last().${option.cy} by page.locator().last().${option.playwright}`, () => {
       const result = converter(`cy.get("selector").last().${option.cy}`);
 
       assert.strictEqual(format(result), format(`await expect(page.locator("selector").last()).${option.playwright}`));
