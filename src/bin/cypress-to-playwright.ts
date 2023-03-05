9b97571f2488540901f1da968057a85499967aab
Jose Cabrera <jose@codium.team>
2023-03-05 19:07:25 +0100

    Add missing files

---
+++ src/bin/cypress-to-playwright.ts
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+import { globSync } from 'glob';
+import { basename, resolve } from 'path';
+import pc from 'picocolors';
+import ora from 'ora';
+
+import { readFileSync, writeFileSync } from 'fs';
+import { index } from '../index.js';
+const args = process.argv.slice(2);
+const [directory] = args;
+const spinner = ora({
+  discardStdin: false,
+  text: '',
+  spinner: 'squareCorners',
+});
+
+const notMigrated: string[] = [];
+let migrated = 0;
+
+validateDirectory(directory);
+spinner.info(`Reading files from: ${directory}`);
+migrateFiles(getFiles(directory));
+spinner.stop();
+printSummary();
+
+function validateDirectory(directory: string) {
+  if (!directory) {
+    spinner.fail(pc.red('Cypress directory is required'));
+    process.exit(1);
+  }
+}
+
+function getFiles(directory: string) {
+  return globSync(`${directory}/**/*.js`, { ignore: `**/node_modules/**` }).filter(
+    (file: string) => !file.endsWith('cypress.config.js')
+  );
+}
+
+function migrateFiles(files: string[]) {
+  files.forEach((file) => {
+    const content = readFileSync(resolve(file), 'utf-8');
+    try {
+      const result = index(content);
+      migrated += 1;
+      spinner.succeed(`File ${basename(file)} migrated`);
+      writeFileSync(file.replace('.js', '.migrated.js'), result, 'utf-8');
+    } catch (e) {
+      spinner.fail(pc.red(`There was an error migrating: ${basename(file)}`));
+      notMigrated.push(file);
+    }
+  });
+}
+
+function printSummary() {
+  console.log(`
+  ${pc.green('- Migrated:')} ${pc.green(migrated)}
+  ${pc.red('- Error:')} ${pc.red(notMigrated.length)}
+      ${notMigrated.map((file) => {
+        return `${pc.gray('- ' + file)}\n`;
+      })}
+  `);
+}
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import ts from 'typescript';
+import { transform } from './core/transform.js';
+
+export function index(code: string) {
+  if (code.trim() === '') return code;
+
+  const sourceFile = ts.createSourceFile('migration.ts', code, ts.ScriptTarget.Latest, true);
+
+  const printer = ts.createPrinter({
+    newLine: ts.NewLineKind.LineFeed,
+  });
+  const transformationResult = ts.transform(sourceFile, [transform]);
+  const transformedSourceFile = transformationResult.transformed[0];
+
+  return printer.printNode(ts.EmitHint.Unspecified, transformedSourceFile, sourceFile);
+}
