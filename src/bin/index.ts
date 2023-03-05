1d2b5e2252b91fcf198e49153779adaac1de10af
Jose Cabrera <jose@codium.team>
2023-03-05 16:20:11 +0100

    Add more logs

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -7,7 +7,8 @@ const { globSync } = require('glob');
 const { resolve } = require('path');
 const pc = require('picocolors');
 const { converter } = require('../converter');
-const notMigrated = [];
+const notMigrated: string[] = [];
+let migrated = 0;
 
 const log = {
   error(message: string) {
@@ -16,21 +17,14 @@ const log = {
   info(message: string) {
     console.log(pc.blue('INFO:') + ' ' + message);
   },
+  success(message: string) {
+    console.log(pc.bgGreen(pc.bold(pc.white('SUCCESS:'))) + ' ' + message);
+  },
 };
 
 validateDirectory(directory);
-const files: string[] = getFiles(directory);
-
-files.forEach((file) => {
-  log.info(`Migrating ${file}...`);
-  const content = readFileSync(resolve(file), 'utf-8');
-  try {
-    const result = converter(content);
-    writeFileSync(file.replace('.js', '.migrated.js'), result, 'utf-8');
-  } catch (e) {
-    notMigrated.push(file);
-  }
-});
+migrateFiles(getFiles(directory));
+printSummary();
 
 function validateDirectory(directory: string) {
   if (!directory) {
@@ -43,4 +37,28 @@ function getFiles(directory: string) {
   return globSync(`${directory}/**/*.js`, { ignore: `**/node_modules/**` }).filter(
     (file: string) => !file.endsWith('cypress.config.js')
   );
+}
+
+function migrateFiles(files: string[]) {
+  files.forEach((file) => {
+    log.info(`Migrating ${file}...`);
+    const content = readFileSync(resolve(file), 'utf-8');
+    try {
+      const result = converter(content);
+      migrated += 1;
+      writeFileSync(file.replace('.js', '.migrated.js'), result, 'utf-8');
+    } catch (e) {
+      notMigrated.push(file);
+    }
+  });
+}
+
+function printSummary() {
+  log.success(`
+        - Migrated: ${migrated}
+        - Error: ${notMigrated.length}
+            ${notMigrated.map((file) => {
+              return `- ${file}\n`;
+            })}
+  `);
 }
