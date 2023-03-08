ff588304e4deba124892bcab25ea22a15e05f9bd
Jose Cabrera <jose@codium.team>
2023-03-08 10:49:00 +0100

    Improve integration sh

--- test/integration.sh
+++ test/integration.sh
@@ -6,6 +6,8 @@ cypress_files="examples/base-cypress-12.7.0"
 
 echo "Building and migrating $cypress_files"
 rm -rf ./examples/playwright
+
+tput setaf 4; echo -e "[Test INFO]: Building code"; tput sgr0
 npm run build &> /dev/null && node lib/bin/cypress-to-playwright.js $cypress_files &> /dev/null
 
 function validateDirectories {
@@ -23,19 +25,19 @@ function validateDirectories {
 validateDirectories
 # Check that both directories exist
 
-tput setaf 4; echo -e "INFO: Executing test: Comparing $base and $actual\n"; tput sgr0
+tput setaf 4; echo -e "[Test INFO]: Executing test: Comparing $base and $actual"; tput sgr0
 # Compare the contents of the two directories
 diff_out=$(diff -r "$base" "$actual")
 
 # If there is no difference, exit with a success message
 if [ -z "$diff_out" ]; then
-  tput setaf 2; echo "Pass: Directories are identical"; tput sgr0
+  tput setaf 2; echo "[Test PASS]: Directories are identical"; tput sgr0
   exit 0
 else
   # Otherwise, output an error message for each file that is different
-  tput setaf 1; echo "Failed:"; tput sgr0
+  tput setaf 1; echo "[Test FAILED]:"; tput sgr0
   echo "$diff_out" | while read line; do
-    echo "Change detected in line: $line"
+    echo -e "- Change detected: $line\n"
   done
   exit 1
 fi
