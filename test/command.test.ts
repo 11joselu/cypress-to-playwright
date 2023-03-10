a529e8f3ede0b479991e8d190ddfe02ea66d1da9
Jose Cabrera <jose@codium.team>
2023-03-10 21:42:25 +0100

    Add more tests for command

--- test/command.test.ts
+++ test/command.test.ts
@@ -3,8 +3,9 @@ import * as command from '../src/command.js';
 import { EmptyDirectoryException } from '../src/core/EmptyDirectoryException.js';
 import { resolve } from 'path';
 import * as fs from 'fs';
+import { format } from './test-utils.js';
 
-describe('Command', () => {
+describe.only('Command', () => {
   const ROOT_DIR = resolve('.', 'test', 'command', 'tmp');
 
   beforeEach(() => {
@@ -26,4 +27,54 @@ describe('Command', () => {
 
     assert.ok(fs.existsSync(resolve(ROOT_DIR, '..', 'playwright')));
   });
+
+  it('Read and converts cypress code', () => {
+    createFileWithContent(
+      resolve(ROOT_DIR, 'aTest.cy.js'),
+      format(`
+      it('Test case', () => { })
+    `)
+    );
+
+    command.execute(ROOT_DIR);
+
+    assert.strictEqual(
+      readFile(resolve(ROOT_DIR, '..', 'playwright', 'aTest.cy.js')),
+      `test('Test case', async ({ page }) => { });\n`
+    );
+  });
+
+  it('Do not load non js file', () => {
+    createFileWithContent(resolve(ROOT_DIR, 'README.md'), '# Title');
+
+    command.execute(ROOT_DIR);
+
+    assert.equal(fs.existsSync(resolve(ROOT_DIR, '..', 'playwright', 'README.md')), false);
+  });
+
+  it('Do not load node_modules files', () => {
+    const node_modules_directory = resolve(ROOT_DIR, 'node_modules');
+    fs.mkdirSync(node_modules_directory, { recursive: true });
+    createFileWithContent(resolve(node_modules_directory, 'library.js'), 'const library = true;');
+
+    command.execute(ROOT_DIR);
+
+    assert.equal(fs.existsSync(resolve(ROOT_DIR, '..', 'playwright', 'node_modules', 'library.js')), false);
+  });
+
+  it('Do not load cypress config file', () => {
+    createFileWithContent(resolve(ROOT_DIR, 'cypress.config.js'), 'const library = true;');
+
+    command.execute(ROOT_DIR);
+
+    assert.equal(fs.existsSync(resolve(ROOT_DIR, '..', 'playwright', 'cypress.config.js')), false);
+  });
 });
+
+function createFileWithContent(file: string, content: string) {
+  fs.writeFileSync(file, content, { flag: 'w' });
+}
+
+function readFile(file: string): string {
+  return fs.readFileSync(file, 'utf-8');
+}
