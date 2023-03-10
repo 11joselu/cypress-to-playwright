49d455e3b7d714d84fb951f81e267744779ebe8e
Jose Cabrera <jose@codium.team>
2023-03-10 13:12:10 +0100

    Fix base playwright code

--- examples/base-playwright/1-getting-started/todo-with-functions.cy.js
+++ examples/base-playwright/1-getting-started/todo-with-functions.cy.js
@@ -1,9 +1,9 @@
 test.describe('example to-do app', () => {
     test.beforeEach(async ({ page }) => {
-        await gotoPage(page);
+        await gotoPage(page)
     })
     test('displays two todo items by default', async ({ page }) => {
-      await validateItemsAsArrowFunction(page);
+        await validateItemsAsArrowFunction(page)
     });
     test('can add new todo items', async ({ page }) => {
         const newItem = 'Feed the cat';
@@ -12,13 +12,11 @@ test.describe('example to-do app', () => {
         await expect(page.locator('.todo-list li').last()).toHaveText(newItem)
     });
 });
-
 async function gotoPage(page) {
-  await page.goto("https://example.cypress.io/todo");
+    await page.goto("https://example.cypress.io/todo")
 }
-
-const validateItemsAsArrowFunction = async(page) => {
-  await expect(page.locator(".todo-list li")).toHaveCount(2);
-  await expect(page.locator(".todo-list li").first()).toHaveText("Pay electric bill");
-  await expect(page.locator(".todo-list li").last()).toHaveText("Walk the dog");
-}
+const validateItemsAsArrowFunction = async (page) => {
+    await expect(page.locator(".todo-list li")).toHaveCount(2)
+    await expect(page.locator(".todo-list li").first()).toHaveText("Pay electric bill")
+    await expect(page.locator(".todo-list li").last()).toHaveText("Walk the dog")
+};
