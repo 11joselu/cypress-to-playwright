8fd34802dde10783f857db83265dc04ac25c785d
Jose Cabrera <jose@codium.team>
2023-03-11 19:34:27 +0100

    Migrate custom command into a normal function injecting page

--- src/core/converters/custom-command.ts
+++ src/core/converters/custom-command.ts
@@ -1,12 +1,17 @@
 import ts from 'typescript';
 import { Factory } from './node-factory.js';
+import { PLAYWRIGHT_PAGE_NAME } from '../playwright.js';
 
 export function handle(node: ts.ExpressionStatement, factory: Factory) {
   const call = node.expression as ts.CallExpression;
   const name = call.arguments[0].getText();
   const callback = call.arguments[1] as unknown as ts.FunctionDeclaration;
 
-  return factory.function(fixString(name), callback.parameters, factory.emptyBlock());
+  return factory.function(
+    fixString(name),
+    [...callback.parameters, factory.parameter(PLAYWRIGHT_PAGE_NAME)],
+    factory.emptyBlock()
+  );
 }
 
 function fixString(str: string) {
--- test/converter.cy-custom-commands.test.ts
+++ test/converter.cy-custom-commands.test.ts
@@ -3,15 +3,15 @@ import { format } from './test-utils.js';
 import assert from 'assert';
 
 describe('Converter: Cypress Custom commands', () => {
-  it('Migrate custom command into a normal function', () => {
+  it('Migrate custom command into a normal function injecting page', () => {
     const result = converter("Cypress.Commands.add('login', () => {})");
 
-    assert.strictEqual(format(result), format('async function login() {}'));
+    assert.strictEqual(format(result), format('async function login(page) {}'));
   });
 
-  it('Migrate custom command with parameters into a normal function with parameters', () => {
+  it('Migrate custom command with parameters into a normal function with parameters injecting page', () => {
     const result = converter("Cypress.Commands.add('login', (username, password) => {})");
 
-    assert.strictEqual(format(result), format('async function login(username, password) {}'));
+    assert.strictEqual(format(result), format('async function login(username, password, page) {}'));
   });
 });
